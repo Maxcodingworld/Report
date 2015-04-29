711e4c33962a5916e83d8c63c672c8fe33a95093
Maxcodingworld <sanyamstrata@gmail.com>
2015-04-29 15:32:19 +0530

    all operations without pagination

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -47,49 +47,51 @@ class Admin < ActiveRecord::Base
     asso.pluralize
   end
 
-#feature 1
-def self.joinstring(table1,table2,whichjoin,flag)     ## table1,table2 are table names
-  Rails.application.eager_load!
-  tablehash = Hash[ActiveRecord::Base.descendants.collect{|c| [c.table_name, c.name]}]
-  reflex1 = tablehash[table1.pluralize].classify.constantize.reflections[table2.pluralize.to_sym]
-  if reflex1 == nil
-   reflex1 = tablehash[table1.pluralize].classify.constantize.reflections[table2.singularize.to_sym]
-  end  
-  reflex2 = tablehash[table2.pluralize].classify.constantize.reflections[table1.pluralize.to_sym]
-  if reflex2 == nil
-    reflex2 = tablehash[table2.pluralize].classify.constantize.reflections[table1.singularize.to_sym]
-  end  
-  asso1 = reflex1.macro.to_s
-  asso2 = reflex2.macro.to_s
-  attrr1 = reflex1.options[:foreign_key]
-  attrr2 = reflex2.options[:foreign_key]
-  if attrr1 == nil
-    attrr1 = "id"
-  end 
-  if attrr2 == nil
-    attrr2 = "id"
-  end 
-  if flag == 1
-    return "#{whichjoin} #{table2.pluralize} on #{table1.pluralize}.#{attrr1} = #{table2.pluralize}.#{attrr2}"
-  else
-    return "#{table1.pluralize} #{whichjoin} #{table2.pluralize} on #{table1.pluralize}.#{attrr1} = #{table2.pluralize}.#{attrr2}"
-  end  
-end
+#Feature 1
+  def self.joinstring(table1,table2,whichjoin,flag)     ## table1,table2 are table names
+    Rails.application.eager_load!
+    tablehash = Hash[ActiveRecord::Base.descendants.collect{|c| [c.table_name, c.name]}]
+    reflex1 = tablehash[table1.pluralize].classify.constantize.reflections[table2.pluralize.to_sym]
+    if reflex1 == nil
+     reflex1 = tablehash[table1.pluralize].classify.constantize.reflections[table2.singularize.to_sym]
+    end  
+    reflex2 = tablehash[table2.pluralize].classify.constantize.reflections[table1.pluralize.to_sym]
+    if reflex2 == nil
+      reflex2 = tablehash[table2.pluralize].classify.constantize.reflections[table1.singularize.to_sym]
+    end  
+    asso1 = reflex1.macro.to_s
+    asso2 = reflex2.macro.to_s
+    attrr1 = reflex1.options[:foreign_key]
+    attrr2 = reflex2.options[:foreign_key]
+    if attrr1 == nil
+      attrr1 = "id"
+    end 
+    if attrr2 == nil
+      attrr2 = "id"
+    end 
+    if flag == 1
+      return " #{whichjoin} #{table2.pluralize} on #{table1.pluralize}.#{attrr1} = #{table2.pluralize}.#{attrr2}"
+    else
+      return " #{table1.pluralize} #{whichjoin} #{table2.pluralize} on #{table1.pluralize}.#{attrr1} = #{table2.pluralize}.#{attrr2}"
+    end  
+  end
 
-def self.multiple_join(table1,joinstr,order)
-  stage1=Class.new
-  stage1=table1.classify.constantize.joins("#{joinstr}").order(order)
-end
 
-def self.select_attr(stage1,selectstr)
-  stage2=Class.new
-  stage2=stage1.select(selectstr)
-end
+  def self.multiple_join(table1,joinstr,order)
+    stage1=Class.new
+    stage1=table1.classify.constantize.joins("#{joinstr}").order(order)
+  end
+
+#Feature 2
+  def self.select_attr(stage1,selectstr)
+    stage2=Class.new
+    stage2=stage1.select(selectstr)
+  end
 
 #Feature 3
-  def self.group_having(groupstr,havingstr,stage2,aggregatestr)
+  def self.group_having(groupstr,havingstr,stage2)
     stage3=Class.new
-    stage3=stage2.group(groupstr).having(havingstr).select(aggregatestr)
+    stage3=stage2.group(groupstr).having(havingstr)
   end
 
 
@@ -109,52 +111,122 @@ end
 
 
 #populating tables
-
   def self.populate(userhash)
-    
     reportobj = Report.new(:description => userhash["description"],:invoke_times => 0)
     reportobj.save!
-    id = reportobj.id 
-
+    
     if userhash["join"] != nil
       userhash["join"].keys.each do |x|
         joinobj = Jointable.new(:report_id => reportobj.id,:table1 => x ,:table2 => userhash["join"][x].first,:whichjoin => userhash["join"][x].last)           
         joinobj.save!
+      end  
     end
 
     if userhash["group"] != nil
       userhash["group"].keys.each do |x|     
         groupobj = Grouptable.new(:report_id => reportobj.id,:table_attribute => x + "." + userhash["group"][x])           
         groupobj.save!    
+      end
     end
 
     if userhash["having"] != nil
       userhash["having"].keys.each do |x|     
-        havingobj = Havingtable.new(:report_id => reportobj.id,:table_attribute => x + "." + userhash["having"][x].first
-          ,:value => userhash["having"][x].first(3).last ,:r_operator => userhash["having"][x].first(2).last 
-          ,:expo_default_flag => userhash["having"][x].last)           
+        havingobj = Havingtable.new(:report_id => reportobj.id,:table_attribute => x + "." + userhash["having"][x].first,:value => userhash["having"][x].first(3).last ,:r_operator => userhash["having"][x].first(2).last,:expo_default_flag => userhash["having"][x].last)           
         havingobj.save!    
+      end
     end
 
     if userhash["order"] != nil
       userhash["order"].keys.each do |x|     
-        orderobj = Ordertable.new(:report_id => reportobj.id,:table_attribute => x + "." + userhash["order"][x].first
-          ,:desc_asce =>userhash["order"][x].first(2).last ,:expo_default_flag => userhash["order"][x].last)           
+        orderobj = Ordertable.new(:report_id => reportobj.id,:table_attribute => x + "." + userhash["order"][x].first,:desc_asce =>userhash["order"][x].first(2).last ,:expo_default_flag => userhash["order"][x].last)
         orderobj.save!    
+      end
     end
 
     if userhash["where"] != nil
       userhash["where"].keys.each do |x|     
-        whereobj = Wheretable.new(:report_id => reportobj.id,:table_attribute => x + "." + userhash["where"][x].first
-          ,:value => userhash["where"][x].first(3).last ,:r_operator => userhash["where"][x].first(2).last 
-          ,:expo_default_flag => userhash["where"][x].last)           
+        whereobj = Wheretable.new(:report_id => reportobj.id,:table_attribute => x + "." + userhash["where"][x].first,:value => userhash["where"][x].first(3).last ,:r_operator => userhash["where"][x].first(2).last,:expo_default_flag => userhash["where"][x].last)
         whereobj.save!    
+      end
     end      
     
     if userhash["select"] != nil
       userhash["select"].keys.each do |x|     
         selectobj = Selecttable.new(:report_id => reportobj.id,:table_attribute => x + "." + userhash["select"][x])           
         selectobj.save!    
+      end
     end 
+    reportobj.id
+  end
+
+# calling multiple_join method
+  def self.join_order_operation(reportobj)
+    var =1
+    joinstr = ""
+    maintable=reportobj.jointables.first.table1
+    reportobj.jointables.each do |x|
+      joinstr << joinstring(x.table1,x.table2,x.whichjoin,var)
+      var = var +1
+    end
+    
+    orderstr = ""
+    reportobj.ordertables.each do |x|
+      orderstr << x.table_attribute + " " + x.desc_asce + ","
+    end
+    orderstr = orderstr[0..orderstr.length-2]  
+    
+    joined_table = multiple_join(maintable,joinstr,orderstr)
+  end
+
+#calling filters method
+  def self.where_operation(joined_table,reportobj)
+    wherestr = []
+    wherestr[0]=""
+    reportobj.wheretables.each do |x|
+       wherestr[0] << x.table_attribute + " " + x.r_operator + " " + "?" + "and"
+       wherestr << x.value
+    end
+    wherestr[0] = wherestr[0][0..wherestr[0].length-4]
+    after_where = filters(joined_table,wherestr)
+  end
+
+#calling group_having method
+  def self.group_having_operations(after_where,reportobj)
+    groupstr = ''     
+    reportobj.grouptables.each do |x|
+       groupstr << x.table_attribute + ","
+    end 
+    groupstr=groupstr[0..groupstr.length-2]
+    havingstr=[]
+    havingstr[0] = ""
+    reportobj.havingtables.each do |x|
+       havingstr[0] << x.table_attribute + " " + x.r_operator + " " + "?" + "and"
+       havingstr << x.value
+    end
+    havingstr[0] = havingstr[0][0..havingstr[0].length-4]
+    after_group_having = group_having(groupstr,havingstr,after_where)
+  end
+  
+  #calling select_attr method 
+  def self.select_operation(after_group_having,reportobj)
+    selectstr = ""
+    reportobj.selecttables.each do |x|
+       selectstr << x.table_attribute + ","
+    end  
+    selectstr = selectstr[0..selectstr.length-2]
+    after_select = select_attr(after_group_having,selectstr)
+  end
+
+
+  def self.retrive_data(id)
+    reportobj=Report.find(id)
+    if id == nil
+      return "Error report doesnot get saved"
+    end
+
+    joined_table = join_order_operation(reportobj)                       # Joining of tables and ordering of table
+    after_where  = where_operation(joined_table,reportobj)               # appling filters on table
+    after_group_having = group_having_operations(after_where,reportobj)  # appling group by and having 
+    after_select = select_operation(after_group_having,reportobj)        # appling select operation
   end
 end
